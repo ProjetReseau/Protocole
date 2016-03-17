9c323f9b3dafbc16d6941c33f09b25611533db64
nlaurent9816 <nicolasLaurent9816@gmail.com>
2016-03-17 21:00:14 +0100

    version 1
    Il s'agit de la version que j'avais, avec dans la structure "taille",
    "type message" et "message" , et dans l'énumération le hello/quit/texte
    et des mots relatifs aux fichiers (peut-être ont-ils été modifiés
    depuis.)

---
+++ protocole.h
@@ -0,0 +1,25 @@
+#define MAX_CONNECTE 20
+#define TAILLE_PSEUDO 20
+#define TAILLE_MAX_MESSAGE 512
+
+
+
+typedef enum type {
+
+  hello,
+  quit,
+  texte,
+  fileProposition,
+  fileAcceptation,
+  fileTransfert
+
+}type;
+
+typedef struct trame {
+
+  int taille; // Taille du truc en octet
+  type type_message; //Message texte ? Fichier ? Audio ? Video ?  
+  //char pseudo_destination[TAILLE_PSEUDO][MAX_CONNECTE];
+  char message[TAILLE_MAX_MESSAGE];
+
+}trame;
