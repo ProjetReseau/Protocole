b1de5f684e8854aed4cb4657cc745c5df45deaf4
Klopfy <klopfenstein_luc@yahoo.fr>
2016-04-20 10:28:35 -0700

    Update protocole.c

--- protocole.c
+++ protocole.c
@@ -5,16 +5,33 @@
 
 
 int tr_to_str(char * message, trame tr){ //retourne la longueur final de message pour avoir la taille à envoyer dans write
- 
-  ApplicationA(message, tr);
-  PresentationA(message, tr);
-  SessionA(message, tr);
+
+  if (tr.type_message==fileTransfert){
+    FileProtocole(message, tr);
+  }
+  else {
+    ApplicationA(message, tr);
+    PresentationA(message, tr);
+    SessionA(message, tr);
+  }
 
   return strlen(message);
 
 }
 
-int ApplicationA(char * message, trame tr){ 
+int FileProtocole(char * message, trame tr){
+
+  sprintf(message, "%i File_T ", tr.taille);
+  char taille[6];
+  sscanf(message, "%s", taille);
+  char * ptr_dest=message+strlen(taille)+strlen("File_T")+1;
+  char * ptr_src=tr.message;
+  memcpy(ptr_dest,ptr_src,TAILLE_MAX_MESSAGE);
+
+  return EXIT_SUCCESS;
+}
+
+int ApplicationA(char * message, trame tr){
 
   switch (tr.type_message){
     case hello:
@@ -24,39 +41,39 @@ int ApplicationA(char * message, trame tr){
     case quit:
       sprintf(message, "QUIT");
       break;
-    
+
     case texte:
       sprintf(message, "MSG %s", tr.message);
       break;
 
     case fileProposition:
-      sprintf(message, "File_P %s", tr.message); 
+      sprintf(message, "File_P %s", tr.message);
       break;
-  
+
     case fileAcceptation:
-      sprintf(message, "File_A %s", tr.message); 
+      sprintf(message, "File_A %s", tr.message);
       break;
-  
+
     case fileTransfert:
-      sprintf(message, "File_T %s", tr.message); 
+      sprintf(message, "File_T %s", tr.message);
       break;
-  
+
     case groupJoin:
-      sprintf(message, "JOIN %s", tr.message); 
+      sprintf(message, "JOIN %s", tr.message);
       break;
-  
+
     case annuaireNew:
-      sprintf(message, "NEW %s", tr.message); 
+      sprintf(message, "NEW %s", tr.message);
       break;
-  
+
     case annuaireAsk:
-      sprintf(message, "ASK %s", tr.message); 
+      sprintf(message, "ASK %s", tr.message);
       break;
-  
+
     case annuaireInfo:
-      sprintf(message, "INFO"); 
+      sprintf(message, "INFO");
       break;
-    
+
     default:
       break;
   }
@@ -71,49 +88,53 @@ int PresentationA(char * message, trame tr){ //à définir
 }
 
 int SessionA(char * message, trame tr){ //Rajoute la taille des données dans le message
-  
+
   char mess[TAILLE_MAX_MESSAGE+32];
-
   sprintf(mess,"%s",message);
-
   sprintf(message, "%i %s", tr.taille, mess);
- 
+
   return EXIT_SUCCESS;
 
-} 
+}
 
 
 int str_to_tr(char * message, trame * tr){
 
-  SessionR(message, tr);
-  PresentationR(message,tr);
-  if (ApplicationR(message, tr)==EXIT_FAILURE){
+  char mot[6];
+  char taille[6];
+  sscanf(message, "%s %s", taille, mot);
+
+  if (strncmp("File_T", mot, 6)==0){
+    memcpy(tr->message, message, TAILLE_MAX_MESSAGE);
   }
   else{
-    printf("Attention tous les paquets émis n'ont pas été reçu... Message à renvoyer\n");
-    return EXIT_FAILURE;
+    SessionR(message, tr);
+    PresentationR(message,tr);
+    if (ApplicationR(message, tr)==EXIT_FAILURE){
+      printf("Attention tous les paquets émis n'ont pas été reçu... Message à renvoyer\n");
+      return EXIT_FAILURE;
+    }
   }
-
   return EXIT_SUCCESS;
 }
 
 int SessionR(char * message, trame * tr){ //Remplit le champ tr.taille
- 
+
    char taille[6];
    char tmp[TAILLE_MAX_MESSAGE+32];
-   
+
    sscanf(message, "%s", taille);
    tr->taille=atoi(taille);
-   
+
    sprintf(tmp, "%s", message+strlen(taille)+1);
    sprintf(message, "%s", tmp);
-  
+
    return EXIT_SUCCESS;
 }
 
 int PresentationR(char * message, trame * tr){ //A définir
-   
-  return EXIT_SUCCESS; 
+
+  return EXIT_SUCCESS;
 
 }
 
@@ -122,7 +143,7 @@ int ApplicationR(char * message, trame * tr){//Remplit le champ tr.message et tr
   char mot[6];
 
   sscanf(message, "%s", mot);
-  
+
   sprintf(tr->message,"%s",message+strlen(mot)+1);
 
   if(strncmp("HELLO", mot, 4)==0){
@@ -155,8 +176,8 @@ int ApplicationR(char * message, trame * tr){//Remplit le champ tr.message et tr
   else if(strncmp("INFO", mot, 4)==0){
     tr->type_message=annuaireInfo;
   }
- 
-  if (sizeof(message)==tr->taille){
+
+  if (strlen(tr->message)==tr->taille){
     return EXIT_SUCCESS;
   }
   else{
